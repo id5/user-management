f449533a2a1b86345d9de88484e0781f75b00b4e
Fernando Souza <fernandovbsouza@gmail.com>
2024-05-27 10:36:31 -0300

    Increase IP column size

---
+++ migrations/m240527_161618_alter_table_user.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Migration;
+
+class m240527_161618_alter_table_user extends Migration
+{
+    
+    public function safeUp()
+    {
+        $this->alterColumn('{{%user_visit_log}}', 'ip', 'VARCHAR(40) NULL');
+    }
+
+    public function safeDown()
+    {
+    }
+    
+}
