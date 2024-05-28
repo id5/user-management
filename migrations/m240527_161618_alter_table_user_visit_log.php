06e476413b4e11e0eb7f49a0040b2580342cfb07
Fernando Souza <fernandovbsouza@gmail.com>
2024-05-28 17:26:58 -0300

    Ajuste tamanho dos campos de ip

--- migrations/m240527_161618_alter_table_user_visit_log.php
+++ migrations/m240527_161618_alter_table_user_visit_log.php
@@ -7,7 +7,7 @@ class m240527_161618_alter_table_user extends Migration
     
     public function safeUp()
     {
-        $this->alterColumn('{{%user}}', 'registration_ip', 'VARCHAR(40) NULL');
+        $this->alterColumn('{{%user_visit_log}}', 'ip', 'VARCHAR(40) NULL');
     }
 
     public function safeDown()
